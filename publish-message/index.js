e07862a68963605966aba09c8ef178732efbbdf4
kaiser <cesar.aguirre@naranja.com>
2020-11-17 07:47:30 -0300

    tarea clase 1

--- publish-message/index.js
+++ publish-message/index.js
@@ -5,13 +5,25 @@ const sqs = new AWS.SQS();
 
 exports.handler = async (event) => {
     try {
+        var name = JSON.parse(event.body).name || null
+        if (name && !ifSpecial(name)){
+            await sqs.sendMessage({
+                MessageBody: JSON.stringify({
+                    name: name
+                }),
+                QueueUrl: 'https://sqs.us-east-1.amazonaws.com/676419150741/colaCKK'
+            }).promise();
+        }else{
+            console.log("No se encontro name o tiene caracteres invalidos")
+            return {
+                statusCode: 500,
+                body: "No se encontro name o tiene caracteres invalidos"
+            };
+        }
+
         // All messages on SQS *MUST* be a String
-        await sqs.sendMessage({
-            MessageBody: JSON.stringify({
-                name: event.name || 'Prueba'
-            }),
-            QueueUrl: 'https://sqs.us-east-1.amazonaws.com/437051718508/Pepe-queue'
-        }).promise();
+
+
     }
     catch (err) {
         console.log(err);
@@ -24,3 +36,14 @@ exports.handler = async (event) => {
 
     return { statusCode: 200 };
 };
+
+function ifSpecial (inputText){
+    let charactersAlan = ["#",'"',"'","!","|","°","¬","%","$","%","&","/","(",")","=",'\\',"<",">","[","]","{","}","^","`"]
+    let detect = false
+    charactersAlan.forEach(character => {
+        if( inputText.indexOf(character) != -1){
+            detect =  true
+        }
+    });
+    return detect
+}
